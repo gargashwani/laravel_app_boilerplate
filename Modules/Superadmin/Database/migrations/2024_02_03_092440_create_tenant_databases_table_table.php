3c6046b7e724bd2fc48430743d30a5446b9e3822
Ashwani Garg <ashwanigarg@outlook.in>
2024-02-03 15:04:56 +0530

    merge

---
+++ Modules/Superadmin/Database/migrations/2024_02_03_092440_create_tenant_databases_table_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tenant_databases', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('tenant_id')->nullable();
+            $table->string('db_url')->nullable();
+            $table->string('db_name')->nullable();
+            $table->string('db_host')->nullable();
+            $table->string('db_user')->nullable();
+            $table->string('db_pass')->nullable();
+            $table->string('server_id')->default('1');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tenant_databases_table');
+    }
+};
--- Modules/User/Database/migrations/2024_02_03_083910_create_role_permissions_table.php
+++ Modules/User/Database/migrations/2024_02_03_083910_create_role_permissions_table.php
@@ -11,10 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('role_permissions', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('role_id')->nullable();
+            $table->bigInteger('module_id')->nullable();
+            $table->enum('enabled',[true, false])->default(false);
+            $table->timestamps();
         });
     }
 
@@ -23,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('role_permissions');
     }
 };
